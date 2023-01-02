8a8ef5cc00265786b8778c56fd9263e96db175b6
S1n1st3r <S1n1st3r@gmail.com>
2023-01-01 19:31:22 -0500

    first commit

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#install owasp amass
+wget https://github.com/OWASP/Amass/releases/download/v3.21.2/amass_linux_amd64.zip
+unzip amass_linux_amd64.zip
+cd amass_linux_amd64
+chmod +x amass
+sudo mv amass /usr/local/bin
+cd ..
+#install JSFScan.sh
+git clone https://github.com/KathanP19/JSFScan.sh.git
+cd JSFScan.sh
+sudo chmod +x install.sh
+./install.sh
+cd ..
+#install nikto
+sudo apt-get install nikto
+#install http probe
+go install github.com/tomnomnom/httprobe@latest
+#install xsrfprobe, clone github.com/0xInfection/XSRFProbe/tree/fixes
+git clone -b fixes https://github.com/0xInfection/XSRFProbe.git
+python3 setup.py install
+#install xsscrapy
+git clone https://github.com/DanMcInerney/xsscrapy.git
+cd xsscrapy
+pip3 install -r requirements.txt
+echo "export PATH=$PATH:$(pwd)" >> ~/.bashrc
+cd ..
+#install nuclei
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+#install SSRFire
+git clone https://github.com/ksharinarayanan/SSRFire.git
+cd SSRFire
+chmod +x setup.sh
+./setup.sh
---
+++ run.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#check for two arguments
+if [ $# -ne 1 ]
+then
+    echo "Usage: ./run.sh <target> optional:<cookie>"
+    exit
+fi
+#Get target/cookie from user agument
+target=$1
+#check if target folder already exsists
+if [ -d $target ]
+then
+    echo "Target folder already exsists, Do you want to overwrite it? (y/n): "
+    #check their response
+    read response
+    if [ $response == "y" ]
+    then
+        #remove the folder
+        rm -rf $target
+    else
+        #exit
+        exit
+    fi
+fi
+#Create a directory for the target
+mkdir $target
+#Change directory to the target
+cd $target
+#Run OWASP Amass to collect subdomains
+#save filename as target-subs.txt
+echo "Running OWASP Amass to collect subdomains"
+amass enum -d $target -o $target-subs.txt
+
+#Test subdomains for alive status
+#save filename as target-alive.txt
+echo "Testing subdomains for alive status"
+cat $target-subs.txt | httprobe >> $target-alive.txt
+
+#collect js links from target main page
+#save filename as jsfile_links.txt
+#if cookie is provided, use it
+echo "Collecting js links from target main page"
+if [ $# -eq 2 ]
+then
+    cat $target | hakrawler -js -cookie $2 -depth 2 -scope subs -plain >> jsfile_links.txt
+else
+    cat $target | hakrawler -js -depth 2 -scope subs -plain >> jsfile_links.txt
+fi
+#Run JSFScan.sh on jsfile_links.txt
+#NOTE: If you feel tool is slow just comment out hakrawler line at 23 in JSFScan.sh script , but it might result in little less jsfileslinks.
+bash JSFScan.sh -l $target --all -r -o $target-JSFScan
+#Run nikto on target-alive.txt
+#save filename as target-nikto.txt
+echo "Running nikto on target-alive.txt"
+cat $target-alive.txt | xargs -n1 -P10 nikto -h >> $target-nikto.txt
+#Run xsrfprobe on target-alive.txt
+#save folder as target-xsrfprobe
+echo "Running xsrfprobe on target-alive.txt"
+cat $target-alive.txt | xargs -n1 -P10 xsrfprobe --malicious --quiet -u >> $target-xsrfprobe
+
+mkdir $target-xsscrapy
+cd $target-xsscrapy
+#if cookie is provided, use it
+echo "Running xsscrapy on target-alive.txt"
+if [ $# -eq 2 ]
+then
+    #Run xsscrapy on target-alive.txt
+    #save filename as target-xsscrapy.txt
+    cat $target-alive.txt | xargs -n1 -P10 python3 xsscrapy.py --cookie $2 -c 20 -u >> $target-xsscrapy.txt
+else
+    #Run xsscrapy on target-alive.txt
+    #save filename as target-xsscrapy.txt
+    cat $target-alive.txt | xargs -n1 -P10 python3 xsscrapy.py -c 20 -u >> $target-xsscrapy.txt
+fi
+cd ..
+#run nuclei on target-alive.txt
+#save filename as target-nuclei.txt
+echo "Running nuclei on target-alive.txt"
+nuclei -list $target-alive.txt -fhr >> $target-nuclei.txt
+#start ngrok in the background, but save output to target-SSRF-ngrok.txt
+echo "Starting ngrok in the background"
+ngrok http 80 > $target-SSRF-ngrok.txt &
+#get url from ngrok output
+url=$(cat $target-SSRF-ngrok.txt | grep "https://[0-9a-z]*\.ngrok.io" -o)
+#Run SSRFire on target-alive.txt
+#save filename as target-SSRFire.txt
+echo "Running SSRFire on target-alive.txt"
+cd ./SSRFire
+cat $target-alive.txt | xargs -n1 -P10 ./ssrfire.sh -s $url -f ../$target-alive.txt -d >> $target-SSRFire.txt
+echo "Done"
